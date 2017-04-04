019379697beca13c07daec4dc50df453cae0650e
Justin Duong <justin.duong53@gmail.com>
2017-04-04 16:43:42 -0400

    Original
    Backup branch

---
+++ client/src/client/agarFood.java
@@ -0,0 +1,20 @@
+package client;
+
+/**
+ * Created by 100588398 on 4/2/2017.
+ */
+public class agarFood {
+
+    int x;
+    int y;
+    int size;
+    String color;
+
+    public agarFood(int x,int y,int size,String color){
+        this.x =x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+
+}
